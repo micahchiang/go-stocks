87e16b2bc83b08f2670f2e7324e45204dd0e9c9c
Micah Chiang <micahkchiang@gmail.com>
2022-02-17 20:26:08 -0600

    use finnhub

--- go.mod
+++ go.mod
@@ -3,3 +3,12 @@ module gostocks
 go 1.17
 
 require github.com/google/go-cmp v0.5.7
+
+require (
+	github.com/Finnhub-Stock-API/finnhub-go/v2 v2.0.9 // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
+	golang.org/x/oauth2 v0.0.0-20210218202405-ba52d332ba99 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+)
--- stockprice/stockprice.go
+++ stockprice/stockprice.go
@@ -1,22 +1,42 @@
-// package that takes an input from stdin and retrieves the current// stock price for that symbol
+// package that takes an input from stdin and retrieves the current
+// stock price for that symbol
 
 package stockprice
 
 import (
     "fmt"
+    "context"
     "bufio"
     "os"
+    "strings"
+    "encoding/json"
+    finnhub "github.com/Finnhub-Stock-API/finnhub-go/v2"
 )
 
 func GetStockPrice() {
+
+    cfg := finnhub.NewConfiguration()
+    cfg.AddDefaultHeader("X-Finnhub-Token", os.Getenv("FH_TOKEN"))
+    finnhubClient := finnhub.NewAPIClient(cfg).DefaultApi
+
     reader := bufio.NewReader(os.Stdin)
     fmt.Print("Enter ticker: ")
-    text, err := reader.ReadString('\n')
+    symbol, err := reader.ReadString('\n')
 
     if err != nil {
         fmt.Printf("Call failed with error: %s. \n ", err)
     } else {
-       fmt.Printf("Retrieving current price for %s\n", text)
+       fmt.Printf("Retrieving current price for %s\n", symbol)
+       quote, _, apierr := finnhubClient.Quote(context.Background()).Symbol(strings.ToUpper(strings.TrimSpace(symbol))).Execute()
+       if apierr != nil {
+           fmt.Printf("API call failed with error: %s. \n", apierr)
+       } else {
+            res, err := json.Marshal(quote)
+            if err != nil {
+                fmt.Printf("error converting json: %s. \n", err)
+            }
+            fmt.Println(string(res))
+       }
     }
 
 }
